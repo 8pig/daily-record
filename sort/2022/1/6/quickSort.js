080c9d5800144c7c752515214e308be96e22d3c0
zhaozhou <zhou773@foxmail.com>
2022-01-06 00:38:33 +0800

    每日一练

---
+++ sort/2022/1/6/quickSort.js
@@ -0,0 +1,33 @@
+Array.prototype.quick = function (){
+
+  const rec = arr => {
+    if(arr.length <= 1){
+      return arr;
+    }
+    const left = []
+    const right = []
+    const mind = arr[0]
+    for (let index = 1; index < arr.length; index++) {
+      if (arr[0] > arr[index]) {
+        left.push(arr[index]);
+      } else {
+        right.push(arr[index])
+      }
+    }
+    return [...rec(left), mind, ...rec(right)]
+
+
+
+  }
+
+  const res = rec(this);
+  res.forEach((value, index) => {
+    this[index] = value;
+  })
+
+
+  console.log(res);
+}
+
+const arr = [5,4,3,2,1,0,100]
+arr.quick()
